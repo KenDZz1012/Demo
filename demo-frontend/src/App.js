5505eea3bfafcc6f2aefec9ed1003d4db2da00fb
kienntviettinlab <kien.nt@viettinlab.com>
2024-12-04 09:28:18 +0700

    update

--- demo-frontend/src/App.js
+++ demo-frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>KenDZzzzzzzzzzz</p>
+        <p>Test CI/CD</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
