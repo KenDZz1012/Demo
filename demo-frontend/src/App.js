6aaf658a4acd7c3783ce0c94a78473d10fa4f7df
kienntviettinlab <kien.nt@viettinlab.com>
2024-12-12 14:44:10 +0700

    update

--- demo-frontend/src/App.js
+++ demo-frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Helll yeahhhhhhhhh</p>
+        <p>Helll test</p>
       </header>
     </div>
   );
