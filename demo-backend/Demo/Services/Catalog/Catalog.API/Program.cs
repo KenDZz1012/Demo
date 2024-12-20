06ba9456bc09478c36045ab5b5164af44623542e
kienntviettinlab <kien.nt@viettinlab.com>
2024-12-20 21:14:39 +0700

    update

--- demo-backend/Demo/Services/Catalog/Catalog.API/Program.cs
+++ demo-backend/Demo/Services/Catalog/Catalog.API/Program.cs
@@ -3,19 +3,19 @@ using Microsoft.Data.SqlClient;
 using Service.Lib.Context;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<DapperContext>();
-builder.Services.AddTransient<ICatalogRepository,CatalogRepository>();
+builder.Services.AddTransient<ICatalogRepository, CatalogRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 app.UseAuthorization();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
 
 app.Run();
