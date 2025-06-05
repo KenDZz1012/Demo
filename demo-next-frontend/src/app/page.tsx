dbef1c86583548cb32ede05e2d32830cd7d51676
llong2195 <nduylong9501@gmail.com>
2025-06-05 14:59:52 +0700

    fix: standardize quotes and formatting in multiple files

--- demo-next-frontend/src/app/page.tsx
+++ demo-next-frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,13 +8,13 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-           width={180}
+          width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
