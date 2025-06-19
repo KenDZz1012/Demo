7acff6552b9be861a3f75301768f2d170af8154d
kiennt3 <kiennt3@phenikaa-x.com>
2025-06-19 15:44:13 +0700

    up

---
+++ demo-backend/Demo/Services/Account/Account.Grpc/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using Account.Grpc.Context;
+using Account.Grpc.Protos;
+using AutoMapper;
+
+
+namespace Account.Grpc.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //User
+            CreateMap<UserModel, User>().ReverseMap();
+        }
+    }
+}
