39869899f3edaf9af8426c63ad66ba713257279a
kiennt3 <kiennt3@phenikaa-x.com>
2025-06-20 14:09:06 +0700

    up

--- demo-backend/Demo/Services/Presence/Presence/Hubs/PresenceHub.cs
+++ demo-backend/Demo/Services/Presence/Presence/Hubs/PresenceHub.cs
@@ -7,15 +7,18 @@ namespace Presence.Hubs
     public class PresenceHub : Hub
     {
         private readonly IConnectionManager _connectionManager;
+        private readonly ILogger<PresenceHub> _logger;
 
-        public PresenceHub(IConnectionManager connectionManager)
+        public PresenceHub(IConnectionManager connectionManager, ILogger<PresenceHub> logger)
         {
             _connectionManager = connectionManager;
+            _logger = logger;
         }
 
         public override async Task OnConnectedAsync()
         {
             var userId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            _logger.LogInformation($"{Context}, {userId}");
             if (!string.IsNullOrEmpty(userId))
             {
                 await _connectionManager.SetUserOnlineAsync(userId, Context.ConnectionId);
--- demo-frontend/src/signalr/presenceConnection.ts
+++ demo-frontend/src/signalr/presenceConnection.ts
@@ -1,9 +1,11 @@
 import * as signalR from '@microsoft/signalr';
-const baseUrl = process.env.REACT_APP_URL_PRESENCE
+
+const baseUrl = process.env.REACT_APP_URL_PRESENCE;
+
 export const createPresenceConnection = async (): Promise<signalR.HubConnection> => {
     const connection = new signalR.HubConnectionBuilder()
         .withUrl(`${baseUrl}/presence`, {
-            withCredentials: true, // cần thiết nếu token trong cookie
+            accessTokenFactory: () => localStorage.getItem("token") || "", // token được truyền vào header Authorization
         })
         .withAutomaticReconnect()
         .configureLogging(signalR.LogLevel.Information)
