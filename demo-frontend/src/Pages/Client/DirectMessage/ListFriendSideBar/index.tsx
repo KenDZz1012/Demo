a48adaffb85f3843ff5178fbe4cbd8e7eacef919
kiennt3 <kiennt3@phenikaa-x.com>
2025-07-18 16:40:02 +0700

    up

---
+++ demo-frontend/src/Pages/Client/DirectMessage/ListFriendSideBar/index.tsx
@@ -0,0 +1,14 @@
+import { Menu } from "antd";
+
+export default function ListFriendSideBar() {
+    return (
+        <Menu
+            className="channel-menu"
+            theme="dark"
+            mode="inline"
+            style={{ backgroundColor: '#2a2c35', color: 'white', border: 'none', borderTopLeftRadius: 20, borderBottomLeftRadius: 20 }}
+        >
+
+        </Menu>
+    );
+}
--- demo-frontend/src/Pages/Client/DirectMessage/index.tsx
+++ demo-frontend/src/Pages/Client/DirectMessage/index.tsx
@@ -1,3 +1,16 @@
+import { Layout } from "antd";
+import ListFriendSideBar from "./ListFriendSideBar";
+const { Sider, Content } = Layout;
+
 export default function DirectMessage() {
-    return (<div>a</div>)
+    return (
+        <Layout style={{ height: '100%' }}>
+            <Sider width={300} style={{ backgroundColor: "#21212a", padding: "10px 0px 10px 10px" }}>
+                <ListFriendSideBar />
+            </Sider>
+            <Content style={{ backgroundColor: "#21212a", padding: "10px 10px 10px 0px" }}>
+
+            </Content>
+        </Layout>
+    )
 }
