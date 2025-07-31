61055b849ae61c7627cb05368442764353206938
kiennt3 <kiennt3@phenikaa-x.com>
2025-07-31 17:27:43 +0700

    up

--- demo-frontend/src/Pages/Client/DirectMessage/AddFriendSideBar/index.tsx
+++ demo-frontend/src/Pages/Client/DirectMessage/AddFriendSideBar/index.tsx
@@ -14,6 +14,7 @@ import { useSelector } from 'react-redux';
 import { selectAuthUser } from 'store/selectors/authSelectors';
 import { userRelationshipStatus } from 'shared';
 import { usePresenceSocket } from 'socket/usePresenceSocket';
+import { generateUUID } from 'utilities';
 
 const { TabPane } = Tabs;
 
@@ -36,7 +37,7 @@ export default function AddFriendSideBar({ friendPending }: { friendPending: Fri
             return [
                 ...prev,
                 {
-                    id: crypto.randomUUID(),
+                    id: generateUUID(),
                     userName: fromUserId,
                     displayName: fromUserId,
                     isSender: false,
--- demo-frontend/src/utilities/index.ts
+++ demo-frontend/src/utilities/index.ts
@@ -25,3 +25,17 @@ export const spreadSearchQuery = (searchQuery: Record<string, any> = {}) => {
 
     return queryParts.length > 0 ? `?${queryParts.join('&')}` : '';
 };
+
+
+export function generateUUID() {
+    if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+        return crypto.randomUUID();
+    }
+
+    // Polyfill
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
