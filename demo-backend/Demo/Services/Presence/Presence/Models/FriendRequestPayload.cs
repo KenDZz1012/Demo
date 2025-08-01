70a2351dd17760524eed893fc92e3d0aadd2ca35
kiennt3 <kiennt3@phenikaa-x.com>
2025-08-01 10:50:44 +0700

    update

--- demo-backend/Demo/Services/Presence/Presence/Models/FriendRequestPayload.cs
+++ demo-backend/Demo/Services/Presence/Presence/Models/FriendRequestPayload.cs
@@ -2,9 +2,9 @@ namespace Presence.Models;
 
 public class FriendRequestPayload
 {
-    public string FromUserName { get; set; }
-    public string FromUserId { get; set; }
-    public string FromUserAvatarUrl { get; set; }
-    public string FromUserDisplayName { get; set; }
-    public string ToUserName { get; set; }
+    public string? FromUserName { get; set; }
+    public string? FromUserId { get; set; }
+    public string? FromUserAvatarUrl { get; set; }
+    public string? FromUserDisplayName { get; set; }
+    public string? ToUserName { get; set; }
 }
