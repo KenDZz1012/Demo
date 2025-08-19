43d5a8381c32abdd003d22751b525239a7ade0c1
kiennt3 <kiennt3@phenikaa-x.com>
2025-08-19 10:22:52 +0700

    up

--- demo-backend/Demo/Library/Service.Lib/obj/Debug/net6.0/Service.Lib.AssemblyInfo.cs
+++ demo-backend/Demo/Library/Service.Lib/obj/Debug/net6.0/Service.Lib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd224d931e004c73e38805b3da88a6daa709f8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41df645cac7ea29bb4908c1069602cee0a08566")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
