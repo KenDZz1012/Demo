5ae114d8aa759ef588ce49f749a978232d2d88e4
kiennt3 <kiennt3@phenikaa-x.com>
2025-08-28 09:13:47 +0700

    up

--- demo-backend/Demo/Services/Channel/Channel.Infrastructure/Data/ChannelContext.cs
+++ demo-backend/Demo/Services/Channel/Channel.Infrastructure/Data/ChannelContext.cs
@@ -26,7 +26,7 @@ public partial class ChannelContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=103.82.25.49;Port=6543;Database=channel;Username=kendz;Password=kienteo1012");
+        => optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("SQL_CONNECTION"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
